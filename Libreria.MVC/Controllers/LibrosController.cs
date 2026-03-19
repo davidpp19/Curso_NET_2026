d533cf8e76093b1934694cb11bffdf1675c2b5d2
davidpp19 <kevdavidpena.06@gmail.com>
2026-03-19 15:18:02 -0500

    Terminar create, delete, details y edit de la Vista Libros, configuración de controller Libros

--- Libreria.MVC/Controllers/LibrosController.cs
+++ Libreria.MVC/Controllers/LibrosController.cs
@@ -2,6 +2,7 @@
 using LibreriaModelo;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Libreria.MVC.Controllers
 {
@@ -17,69 +18,118 @@ namespace Libreria.MVC.Controllers
         // GET: LibrosController/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            var libro = CRUD<Libro>.GetById(id);
+            if (libro == null)
+            {
+                return NotFound();
+            }
+
+            return View(libro);
+        }
+
+        //Método interno para obtener las bibliotecas, es un GET BIBLIOTECAS.
+        private List<SelectListItem> GetBibliotecas()
+        {
+            var bibliotecas = CRUD<Biblioteca>.GetAll();
+            return bibliotecas.Select(b => new SelectListItem
+            {
+                Value = b.Id.ToString(),
+                Text = b.Nombre_Biblioteca
+            }).ToList();
+        }
+
+        //Método interno para obtener los autores, es un GET AUTORES.
+        private List<SelectListItem> GetAutores()
+        {
+            var autores = CRUD<Autor>.GetAll();
+            return autores.Select(a => new SelectListItem
+            {
+                Value = a.Id.ToString(),
+                Text = $"{a.Nombres_Autor} {a.Apellidos_Autor}"
+            }).ToList();
         }
 
         // GET: LibrosController/Create
         public ActionResult Create()
         {
+            ViewBag.Autores = GetAutores();
+            ViewBag.Bibliotecas = GetBibliotecas();
             return View();
         }
 
         // POST: LibrosController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult Create(Libro libro)
         {
             try
             {
+                CRUD<Libro>.Create(libro);
                 return RedirectToAction(nameof(Index));
             }
-            catch
+            catch (Exception ex)
             {
-                return View();
+                ModelState.AddModelError("", ex.Message);
+                return View(libro);
             }
         }
 
         // GET: LibrosController/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            var libro = CRUD<Libro>.GetById(id);
+            ViewBag.Autores = GetAutores();
+            ViewBag.Bibliotecas = GetBibliotecas();
+
+            if(libro == null)
+            {
+                return NotFound();
+            }
+            return View(libro);
         }
 
         // POST: LibrosController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit(int id, Libro libro)
         {
             try
             {
+                CRUD<Libro>.Update(id, libro);
                 return RedirectToAction(nameof(Index));
             }
-            catch
+            catch (Exception ex)
             {
-                return View();
+                ModelState.AddModelError("", ex.Message);
+                return View(libro);
             }
         }
 
         // GET: LibrosController/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+            var libro = CRUD<Libro>.GetById(id);
+            if (libro == null)
+            {
+                return NotFound();
+            }
+            return View(libro);
         }
 
         // POST: LibrosController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public ActionResult Delete(int id, Libro libro)
         {
             try
             {
+                CRUD<Libro>.Delete(id);
                 return RedirectToAction(nameof(Index));
             }
-            catch
+            catch (Exception ex) 
             {
-                return View();
+                ModelState.AddModelError("", ex.Message);
+                return View(libro);
             }
         }
     }
