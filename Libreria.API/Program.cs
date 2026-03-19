fe652c6c00fa97b0ebf27ca750a1d79081fe97a4
davidpp19 <kevdavidpena.06@gmail.com>
2026-03-19 11:50:13 -0500

    Agregar nueva funcion para Autores para poder escoger pais mas no su id

--- Libreria.API/Program.cs
+++ Libreria.API/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Libreria.API.Data;
+
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<LibreriaAPIContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("LibreriaAPIContext") ?? throw new InvalidOperationException("Connection string 'LibreriaAPIContext' not found.")));
--- Libreria.MVC/Controllers/AutoresController.cs
+++ Libreria.MVC/Controllers/AutoresController.cs
@@ -2,6 +2,7 @@
 using LibreriaModelo;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Libreria.MVC.Controllers
 {
@@ -17,68 +18,104 @@ namespace Libreria.MVC.Controllers
         // GET: AutoresController/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            var autor = CRUD<Autor>.GetById(id);
+            if(autor == null)
+            {
+                return NotFound();
+            }
+            return View(autor);
+        }
+
+        //Método para obtener los paises, es un GET PAISES.
+        private List<SelectListItem> GetPaises()
+        {
+            var paises = CRUD<Pais>.GetAll();
+            return paises.Select(p => new SelectListItem
+            {
+                Value = p.Id.ToString(),
+                Text = p.Nombre_Pais
+            }).ToList();
         }
 
         // GET: AutoresController/Create
         public ActionResult Create()
         {
+            ViewBag.Paises = GetPaises();
             return View();
         }
 
         // POST: AutoresController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult Create(Autor autor)
         {
             try
             {
+                CRUD<Autor>.Create(autor);
                 return RedirectToAction(nameof(Index));
             }
-            catch
+            catch (Exception ex) 
             {
-                return View();
+                ModelState.AddModelError("", ex.Message);
+                return View(autor);
             }
         }
 
         // GET: AutoresController/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            var autor = CRUD<Autor>.GetById(id);
+            ViewBag.Paises = GetPaises();
+
+            if(autor == null)
+            {
+                return NotFound();
+            }
+            return View(autor);
         }
 
         // POST: AutoresController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit(int id, Autor autor)
         {
             try
             {
+                CRUD<Autor>.Update(id, autor);
                 return RedirectToAction(nameof(Index));
             }
-            catch
+            catch (Exception ex)
             {
-                return View();
+                ModelState.AddModelError("", ex.Message);
+                return View(autor);
             }
         }
 
         // GET: AutoresController/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+            var autor = CRUD<Autor>.GetById(id);
+
+            if(autor == null)
+            {
+                return NotFound();
+            }
+            return View(autor);
         }
 
         // POST: AutoresController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public ActionResult Delete(int id, Autor autor)
         {
             try
             {
+                CRUD<Autor>.Delete(id);
                 return RedirectToAction(nameof(Index));
             }
-            catch
+            catch (Exception ex) 
             {
+                ModelState.AddModelError("", ex.Message);
                 return View();
             }
         }
